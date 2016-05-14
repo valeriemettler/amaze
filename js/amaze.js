f6c580becefbd8ab289875c16ddf6ab8e55d6d17
Valerie Mettler <valerie.mettler@swirl.net>
2016-05-13 18:37:41 -0700

    added function to create an array of 10 digits of 0s and 1s

--- js/amaze.js
+++ js/amaze.js
@@ -1,27 +1,32 @@
 //an array with 10 random numbers
 
-var num;
+// var num;
 var arr = [];
 
-var random_number_generator = function(){
-    num = Math.floor(Math.random() * 100) + 1;
-    console.log("num", num);
-    return num;
+// var random_number_generator = function(){
+//     num = Math.floor(Math.random() * 100) + 1;
+//     console.log("num", num);
+//     return num;
+// };
+// var b = random_number_generator();
+// console.log("b", b);
+
+
+// var random_array_generator = function() {
+//     for (var i=0, n=10; i<n; i++) {
+//     arr.push(Math.round(Math.random() * n));
+//    }
+// };
+// random_array_generator();
+// console.log("arr", arr);
+
+var one_zero_generator = function() {
+    for (var i=0; i<10; i++){
+        arr.push((Math.floor(Math.random() * 9) % 2));
+    }
 };
-var b = random_number_generator();
-console.log("b", b);
-
-
-
-
-var random_array_generator = function() {
-    for (var i=0, n=10; i<n; i++) {
-    arr.push(Math.round(Math.random()* n));
-   }
-}
-random_array_generator();
+one_zero_generator();
 console.log("arr", arr);
-
 
 //generates a random whole number between 1 and 100
 // var rand_num = Math.floor(Math.random() * 100) + 1;
