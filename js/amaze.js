11f0c13ca039520c7be69c814f965e33f2f9ee1e
Valerie Mettler <valerie.mettler@swirl.net>
2016-05-14 09:57:15 -0700

    refactored code to combine display functions and remove global array variable so all functions return something

--- js/amaze.js
+++ js/amaze.js
@@ -1,44 +1,33 @@
-var arr = [];
-var current_number = 0;
+var current_number = 1;
 var n = 9;
 
 var one_zero_arr_generator = function() {
-    for (var i=0; i<10; i++){
+    var arr = [];
+    for (var i = 0; i < 10; i++) {
         arr.push((Math.floor(Math.random() * 9) % 2));
     }
+    console.log(arr);
+    return arr;
 };
 one_zero_arr_generator();
-console.log("arr", arr);
 
-var style_display = function(current_number) {
+
+var display = function(current_number, n) {
+    var x = "";
+    for (var i = 0; i <= n; i++) {
+        x = x + '<div class="x" id="' + i + '"></div>';
+    }
+    $('#container').html(x);
     $('.x').css({
-            "border": "1px solid black",
-            "height": "25px",
-            "width": "25px",
-            "float": "left"
-        });
-    if(current_number == 1){
-        // add grey box
+        "border": "1px solid black",
+        "height": "25px",
+        "width": "25px",
+        "float": "left"
+    });
+    if (current_number == 1) {
         $('.x').css({
             "background-color": "grey"
         });
     }
 };
-
-var display = function(current_number,n) {
-
-    var x = "";
-    for (var i=0; i<=n; i++){
-    x = x + '<div class="x"></div>';
- }
- $('#container').html(x);
- style_display(current_number);
-};
-display(current_number,n);
-
-
-
-//create a function that will create n boxes (loop)...function takes n as
-//parameter and call function n times
-
-
+display(current_number, n);
