25dcdcc8bd185264a863bca96c323706a8a4402d
Valerie Mettler <valerie.mettler@swirl.net>
2016-05-14 12:05:06 -0700

    added button to generate maze on click, restyled maze

--- js/amaze.js
+++ js/amaze.js
@@ -1,9 +1,17 @@
+//make a generate maze button that calls the display function
+
+var setHandlers = function() {
+    $(".btn").on('click', function(event) {
+        event.stopPropagation();
+        display();
+    });
+}
+
 var one_zero_arr_generator = function() {
     var arr = [];
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 100; i++) {
         arr.push((Math.floor(Math.random() * 9) % 2));
     }
-    console.log(arr);
     return arr;
 };
 
@@ -11,8 +19,11 @@ var display = function() {
     var a = one_zero_arr_generator();
     var x = "";
     for (var i = 0; i < a.length; i++) {
-        x = x + '<div class="y x' + a[i] + '" id="box' + i + '"></div>';
+        x = x + '<div class="y x' + a[i] + '" id="box' + i + '">'+ i +'</div>';
     }
     $('#container').html(x);
 };
-display();
+
+$(document).ready(function() {
+    setHandlers();
+});
