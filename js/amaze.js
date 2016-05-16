652e78a144238339521bd907bff7a36ef0052766
Valerie Mettler <valerie.mettler@swirl.net>
2016-05-15 20:11:17 -0700

    committing changes before restructuring code for color coding players paths and players scores

--- js/amaze.js
+++ js/amaze.js
@@ -5,6 +5,12 @@ var score = max_score;
 var box_color;
 var player_number = 1;
 var score_table_array = [];
+var random_color_array = ["#fbb735", "#e98931", "#eb403b", "#b32E37", "#6c2a6a",
+    "#5c4399", "#274389", "#1f5ea8", "#227FB0", "#2ab0c5",
+    "#39c0b3","#ffcccc", "#ffe0cc", "#ffeacc", "#fff4cc", "#fffecc",
+    "#effac8", "#c7f5c4", "#c4f0f4", "#c4daf4", "#c9c4f4",
+    "#e1c4f4", "#f6c6e6"];
+var random_color_index = 0;
 
 var setHandlers = function() {
     $(".generate").on('click', function(event) {
@@ -13,6 +19,7 @@ var setHandlers = function() {
         stop = false;
         score = max_score;
         display_score();
+        random_color_generator();
     });
     $(".reset").on('click', function(event) {
         event.stopPropagation();
@@ -22,6 +29,7 @@ var setHandlers = function() {
         $('#box99').removeClass('red active');
         red_box_position = 0;
         stop = false;
+        random_color_generator();
     });
 
     $(".right").on('click', function(event) {
@@ -73,32 +81,64 @@ var one_zero_arr_generator = function() {
     return arr;
 };
 
+var random_color_generator = function() {
+    // random_color = random_color_array[Math.floor(Math.random()*23)];
+    random_color_index = random_color_index + 1;
+};
+
 var display_maze = function() {
     red_box_position = 0;
     var a = one_zero_arr_generator();
     var x = "";
     for (var i = 0; i < a.length; i++) {
+        // console.log((i % 10));
         x = x + '<div class="y x' + a[i] + '" id="box' + i + '">' + i + '</div>';
     }
     $('#container').html(x);
     $('#box0').addClass('red active');
     $('#box99').addClass('green');
+    random_color_generator();
 };
 
 var display_score = function() {
-    $('#score').html(score);
+    $('#score').html("Score " + score);
 };
 
 var display_score_board = function() {
     score_table_array.push(score);
     var a = score_table_array;
     var score_table = "";
-    for (var i = 0; i < a.length ; i++) {
-         score_table = score_table + '<div class="playernumber"> Player ' + (i + 1) +
-         '</div><div class="playerscore">' + score_table_array[i] + '</div>';
+    for (var i = 0; i < a.length; i++) {
+        score_table = score_table + '<div class="playernumber"> Player ' + (i + 1) +
+            '</div><div class="playerscore ps'+ (i+1) +'">' + score_table_array[i] + '</div>';
     }
     $('#scoreboard').html(score_table);
+
+    score_board_style();
     player_number = player_number + 1;
+};
+
+var score_board_style = function() {
+ //    for (var i = 1; i <= player_number; i++) {
+ //        var b = '.ps' + (i);
+ //        console.log(b);
+ // console.log(document.getElementsByClassName(b));
+ // var m = document.getElementsByClassName(b);
+ // var c = m.style;
+ // c.backgroundColor = "#eee";
+
+    // var m = document.getElementById('.ps' + (i+1)), c = m.style;
+    // c.backgroundColor = random_color_array[random_color_index];
+// }
+
+     for (var i = 0; i <= player_number; i++) {
+        console.log("setting the color of:");
+        console.log('.ps' + (i+1));
+        console.log(random_color_array[i]);
+     $('.ps' + (i+1)).first().css ({
+        "background-color": random_color_array[i]
+    });
+ }
 };
 
 var move_right = function() {
@@ -111,6 +151,9 @@ var move_right = function() {
         $('#box' + red_box_position).removeClass('red active');
         red_box_position = red_box_position + 1;
         $('#box' + red_box_position).addClass('red active');
+        $('#box' + red_box_position).css({
+            "background-color": random_color_array[random_color_index]
+        });
     }
     update_score();
     display_score();
@@ -129,6 +172,9 @@ var move_left = function() {
         $('#box' + red_box_position).removeClass('red active');
         red_box_position = red_box_position - 1;
         $('#box' + red_box_position).addClass('red active');
+        $('#box' + red_box_position).css({
+            "background-color": random_color_array[random_color_index]
+        });
     }
     update_score();
     display_score();
@@ -147,7 +193,9 @@ var move_down = function() {
         $('#box' + red_box_position).removeClass('red active');
         red_box_position = red_box_position + 10;
         $('#box' + red_box_position).addClass('red active');
-
+        $('#box' + red_box_position).css({
+            "background-color": random_color_array[random_color_index]
+        });
     }
     update_score();
     display_score();
@@ -166,6 +214,9 @@ var move_up = function() {
         $('#box' + red_box_position).removeClass('red active');
         red_box_position = red_box_position - 10;
         $('#box' + red_box_position).addClass('red active');
+        $('#box' + red_box_position).css({
+            "background-color": random_color_array[random_color_index]
+        });
 
     }
     update_score();
